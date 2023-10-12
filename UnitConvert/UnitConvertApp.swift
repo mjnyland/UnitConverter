532241c0ea7a02a54e9519b6887c2e7f924ddce3
Michael <88445485+mjnyland@users.noreply.github.com>
2023-10-12 19:06:35 -0400

    Initial Commit

---
+++ UnitConvert/UnitConvertApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConvertApp.swift
+//  UnitConvert
+//
+//  Created by Michael Nyland on 10/12/23.
+//
+
+import SwiftUI
+
+@main
+struct UnitConvertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
